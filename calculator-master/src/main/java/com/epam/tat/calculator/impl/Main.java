99f29cb653cd599f5b8170b4e7fe2f3c0b9eea0a
Nad K <nadinka121@rambler.ru>
2022-01-17 21:27:58 +0300

    Add loop of program

--- calculator-master/src/main/java/com/epam/tat/calculator/impl/Main.java
+++ calculator-master/src/main/java/com/epam/tat/calculator/impl/Main.java
@@ -7,39 +7,47 @@ public class Main {
         int precision;
         String expression;
         double[] arrToAction;
-        System.out.println("Enter the precision of calculate");
-        try {
+        String choose ="1";
         Scanner in = new Scanner(System.in);
-        precision = in.nextInt();
-            if (precision >= 0) {
-                Calculator calc = new Calculator(precision);
-                System.out.println("Enter the expression for calculate");
-                expression = in.next();
-                if (calc.checkExpression(expression, calc.getRegex())) {
+        do {
+                    System.out.println("Enter the precision of calculate");
+                    try {
+                        precision = in.nextInt();
+                        if (precision >= 0) {
+                            Calculator calc = new Calculator(precision);
+                            System.out.println("Enter the expression for calculate");
+                            expression = in.next();
+                            if (calc.checkExpression(expression, calc.getRegex())) {
 
-                    arrToAction = calc.stringToDouble(expression.split(calc.searchAction(expression)));
-                    switch (calc.searchAction(expression)) {
-                        case "\\+":
-                            System.out.println(calc.add(arrToAction[0], arrToAction[1]));
-                            break;
-                        case "\\-":
-                            System.out.println(calc.subtract(arrToAction[0], arrToAction[1]));
-                            break;
-                        case "\\*":
-                            System.out.println(calc.multiply(arrToAction[0], arrToAction[1]));
-                            break;
-                        case "\\/":
-                            if (arrToAction[1] != 0) {
-                                System.out.println(calc.divide(arrToAction[0], arrToAction[1]));
-                                break;
+                                arrToAction = calc.stringToDouble(expression.split(calc.searchAction(expression)));
+                                switch (calc.searchAction(expression)) {
+                                    case "\\+":
+                                        System.out.println(calc.add(arrToAction[0], arrToAction[1]));
+                                        break;
+                                    case "\\-":
+                                        System.out.println(calc.subtract(arrToAction[0], arrToAction[1]));
+                                        break;
+                                    case "\\*":
+                                        System.out.println(calc.multiply(arrToAction[0], arrToAction[1]));
+                                        break;
+                                    case "\\/":
+                                        if (arrToAction[1] != 0) {
+                                            System.out.println(calc.divide(arrToAction[0], arrToAction[1]));
+                                            break;
+                                        } else System.out.println("Incorrect input");
+                                }
                             } else System.out.println("Incorrect input");
+                        } else System.out.println("Incorrect input");
+                    } catch (Exception e) {
+                        System.out.println("Incorrect input");
                     }
-                } else System.out.println("Incorrect input");
-            } else System.out.println("Incorrect input");
-        }
-        catch (Exception e) {
-            System.out.println("Incorrect input");
-        }
+                    System.out.println("Enter 1 to continue or 2 to exit");
+                    choose = in.next();
+            }
+        while (choose.equals("1"));
         }
 
-}
+
+    }
+
+
